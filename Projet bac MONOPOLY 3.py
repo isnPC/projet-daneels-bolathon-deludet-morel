9fff33e2b85c0e8f4544234f742a70d27c35c77b
thomas-morel <60392851+thomas-morel@users.noreply.github.com>
2020-04-14 09:02:20 +0200

    Add files via upload

--- Projet bac MONOPOLY 3.py
+++ Projet bac MONOPOLY 3.py
@@ -8,68 +8,40 @@ canvas = Canvas(fenetre, width=1450, height=700,background='black')
 canvas.pack()
 
 for ligne in range(1, 13):
-  if ligne == 2:
+  if ligne in [2, 3, 4]:
     coul = 'blue2'
-  elif ligne == 3:
-    coul = 'blue2'
-  elif ligne == 4:
-    coul = 'blue2'
-  elif ligne == 6:
+  elif ligne in [6, 7]:
     coul = 'yellow'
-  elif ligne == 7:
-    coul = 'yellow'
-  elif ligne == 9:
-    coul = 'Deeppink2'
-  elif ligne == 10:
-    coul = 'Deeppink2'
-  elif ligne == 11:
+  elif ligne in [9, 10, 11]:
     coul = 'Deeppink2'
   else:
     coul = 'white'
   canvas.create_rectangle(100*ligne, 0, 100*ligne+100, 100,fill=coul)
 
 for ligne in range(1, 13):
-  if ligne == 2:
+  if ligne in [2, 3, 4]:
     coul = 'cyan'
-  elif ligne == 3:
-    coul = 'cyan'
-  elif ligne == 4:
-    coul = 'cyan'
-  elif ligne == 6:
+  elif ligne in [6, 7, 8]:
     coul = 'grey'
-  elif ligne == 7:
-    coul = 'grey'
-  elif ligne == 8:
-    coul = 'grey'
-  elif ligne == 10:
-    coul = 'orange'
-  elif ligne == 11:
+  elif ligne in [10, 11]:
     coul = 'orange'
   else:
     coul = 'white'
   canvas.create_rectangle(100*ligne, 600, 100*ligne+100, 700,fill=coul)
 
 for colonne in range(1, 6):
-  if colonne == 1:
+  if colonne in [1, 2]:
     coul = 'red'
-  elif colonne == 2:
-    coul = 'red'
-  elif colonne == 4:
-    coul = 'purple'
-  elif colonne == 5:
+  elif colonne in [4, 5]:
     coul = 'purple'
   else:
     coul = 'white'
   canvas.create_rectangle(100, 100*colonne, 200, 100*colonne+100,fill=coul)
 
 for colonne in range(1, 6):
-  if colonne == 1:
+  if colonne in [1, 2]:
     coul = 'brown'
-  elif colonne == 2:
-    coul = 'brown'
-  elif colonne == 4:
-    coul = 'green'
-  elif colonne == 5:
+  elif colonne in [4, 5]:
     coul = 'green'
   else:
     coul = 'white'
